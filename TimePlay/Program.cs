cb460a83cd1058d7464306602b74d82f65a06dad
NewMoon <milkyboy12081208@gmail.com>
2023-08-14 17:28:52 +0900

    TimeRecharger v1

---
+++ TimePlay/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TimePlay;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TimeZone.CurrentTimeZone.ToLocalTime();
+        //Console.WriteLine(DateTime.Now);
+        //long unixTimeInMillisec = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        //Console.WriteLine(unixTimeInMillisec);
+        //DateTime d = FromUnixTime(unixTimeInMillisec);
+        //Console.WriteLine(d);
+
+        TimeRecharger timeRecharger = new TimeRecharger();
+        Console.WriteLine($" 에너지 {timeRecharger.GetEnergy()}");
+        for(int i=0; i < 2;i++)
+        {
+            timeRecharger.SpendEnergy();
+        }
+        ConsoleKeyInfo cki;
+        while (true)
+        {
+            //cki = Console.ReadKey(true);
+            //switch (cki.Key)
+            //{
+            //    case ConsoleKey.LeftArrow:
+            //        break;
+            //    case ConsoleKey.RightArrow:
+            //        timeRecharger.SpendEnergy();
+            //        Console.WriteLine($" 에너지 {timeRecharger.GetEnergy()}");
+            //        break;
+            //    case ConsoleKey.UpArrow:
+            //        break;
+            //    case ConsoleKey.DownArrow:
+            //        break;
+            //    case ConsoleKey.Q:
+            //        return;
+            //}
+            Thread.Sleep(1000);
+            Console.WriteLine($" 에너지 {timeRecharger.GetEnergy()} , {timeRecharger._Now} , {timeRecharger._timestamp}");
+        }
+
+    }
+
+    static public DateTime FromUnixTime(long second)
+    {
+        DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        return epoch.AddMilliseconds(second);
+    }
+}
---
+++ TimePlay/TimeRecharger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimePlay
+{
+    public class TimeRecharger
+    {
+        public long _timestamp;
+        public long _Now {
+            get { return GetEpoch(DateTimeOffset.UtcNow); }
+        }
+
+        public TimeRecharger() 
+        {
+            //new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var datetime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var dateWithOffset = new DateTimeOffset(datetime).ToUniversalTime();
+            _timestamp = dateWithOffset.ToUnixTimeMilliseconds();
+        }
+        const double MAX_ENERGY = 100;
+        const long RECHARGE_CYCLE = 60000;//1000 = 1초
+
+        public int remain_energy = 0;
+        public long remaining_time_to_full = 0;
+        public long remaining_time_to_one = 0;
+
+        public long GetEpoch(DateTimeOffset date)
+        {
+            var dateWithOffset = date.ToUniversalTime();
+            return dateWithOffset.ToUnixTimeMilliseconds();
+        }
+        public long GetEpoch(DateTime date)
+        {
+            var dateWithOffset = new DateTimeOffset(date).ToUniversalTime();
+            return dateWithOffset.ToUnixTimeMilliseconds();
+        }
+        public int GetEnergy()
+        {
+            var epochNow = GetEpoch(DateTimeOffset.UtcNow);
+
+            double energy = ((epochNow - _timestamp) / RECHARGE_CYCLE);
+            energy = MAX_ENERGY <= energy ? MAX_ENERGY : energy;
+
+            remaining_time_to_full = (long)(MAX_ENERGY <= energy ?
+                0 : RECHARGE_CYCLE * MAX_ENERGY - (epochNow - _timestamp));
+            remaining_time_to_one = remaining_time_to_full % RECHARGE_CYCLE;
+            bool a = remaining_time_to_full != 0;
+            bool b = remaining_time_to_one != 0;
+
+            remaining_time_to_one = a && !b?
+                RECHARGE_CYCLE : remaining_time_to_full % RECHARGE_CYCLE;
+
+            remain_energy = (int)energy;
+            return (int)energy;
+        }
+
+        public void SpendEnergy()
+        {
+            var epochNow = GetEpoch(DateTimeOffset.UtcNow);
+
+            double energy = ((epochNow - _timestamp) / RECHARGE_CYCLE);
+            energy = MAX_ENERGY <= energy ? MAX_ENERGY : energy;
+
+            _timestamp = (long)(MAX_ENERGY == energy ?
+                epochNow - RECHARGE_CYCLE * (MAX_ENERGY - 1) :
+                _timestamp + RECHARGE_CYCLE * 1);
+        }
+    }
+}
